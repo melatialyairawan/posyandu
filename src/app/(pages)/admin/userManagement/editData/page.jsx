b33c7d8c1ed5fa1ef9fdbb004f00e2f238363927
Adam <105685257+AdamAjiLangit@users.noreply.github.com>
2024-12-11 18:57:55 +0700

    feat : [AD] user-management

--- src/app/(pages)/admin/userManagement/editData/page.jsx
+++ src/app/(pages)/admin/userManagement/editData/page.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { IoIosArrowDropleft } from 'react-icons/io';
+import { EditUserForm } from '@/components/UserManagement/EditData/EditUser';
 
 const EditUser = () => {
     return (
-        <div>EditUser</div>
+        <div>
+            <Link href="/admin/userManagement" className='flex items-center gap-2 mb-5 text-[#7E84A3]'>
+                <IoIosArrowDropleft size={18} />
+                <h3 className='text-base font-semibold'>Kembali</h3>
+            </Link>
+            <h2 className='font-semibold text-xl mb-5'>Edit Data</h2>
+            <EditUserForm />
+        </div>
     )
 }
 
