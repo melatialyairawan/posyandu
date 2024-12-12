2ca31aff643c366fe9965c1a96c09fbc990c264f
hafizbima <mas.bima.hafiz@gmail.com>
2024-12-12 10:19:51 +0700

    First commit

--- src/app/(pages)/admin/dashboard/page.jsx
+++ src/app/(pages)/admin/dashboard/page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Chart from '@/components/NutritionChart/Chart';
+import DashboardTable from '@/components/TableComponent/DashboardTable';
+
 
 const Dashboard = () => {
-    return (
-        <div>Dashboard</div>
-    )
-}
+  return (
+    <div>
+      <h2 className='font-semibold text-xl mb-5'>Beranda</h2>
+      <h2 className='font-semibold text-xl mb-5 ml-10'>Status Gizi</h2>
+      <Chart />
+      <h2 className='font-semibold text-xl mt-10 ml-10'>Data Pengunjung</h2>
+      <DashboardTable />
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
---
+++ src/components/TableComponent/DashboardTable.jsx
@@ -0,0 +1,127 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import {
+    Table,
+    TableHeader,
+    TableBody,
+    TableColumn,
+    TableRow,
+    TableCell,
+    Button,
+    Input,
+    Badge,
+    Checkbox,
+    Pagination,
+    Skeleton,
+    Select,
+    SelectItem,
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    useDisclosure,
+} from '@nextui-org/react';
+import { LuPencilLine, LuTrash } from "react-icons/lu";
+import toast from 'react-hot-toast';
+
+export default function InspectionHistoryTable() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const router = useRouter();
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoaded(true);
+        }, 1500);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+   
+    const handleToEdit = () => {
+        const loadingToast = toast.loading('Loading...');
+        try {
+            router.push('/admin/dashboard/addActivity');
+            toast.success('Redirecting...');
+        } catch (error) {
+            toast.error('Navigation failed');
+        } finally {
+            toast.dismiss(loadingToast);
+        }
+    }
+
+    const data = [
+        { namaposyandu: 'Posyandu Seroja', tanggal: '09-10-2024', jumlahpengunjung: '24', alamat: 'Getas Pejaten RT 00/RW 00, Jati, Kudus', },
+        { namaposyandu: 'Posyandu Melati 5', tanggal: '19-11-2024', jumlahpengunjung: '27', alamat: 'Jati Wetan RT 01/RW 02, Jati, Kudus', },
+    ];
+
+    return (
+        <div className="">
+            <div className="flex justify-between items-center mb-5">
+               
+            </div>
+
+            <Table
+                aria-label="Example table with dynamic content"
+                className="w-full border border-gray-200 rounded-2xl shadow-md"
+            >
+                <TableHeader>
+                    <TableColumn> </TableColumn>
+                    <TableColumn>Nama Posyandu</TableColumn>
+                    <TableColumn>Tanggal</TableColumn>
+                    <TableColumn>Jumlah Pengunjung</TableColumn>
+                    <TableColumn>Alamat</TableColumn>
+                    <TableColumn>Aksi</TableColumn>
+                </TableHeader>
+                <TableBody>
+                    {data.map((item, index) => (
+                        <TableRow key={index} className="hover:bg-gray-100">
+                            <TableCell>
+                                <Skeleton isLoaded={isLoaded}>
+                                    <Checkbox />
+                                </Skeleton>
+                            </TableCell>
+                            <TableCell>
+                                <Skeleton isLoaded={isLoaded}>
+                                    {item.namaposyandu}
+                                </Skeleton>
+                            </TableCell>
+                            <TableCell>
+                                <Skeleton isLoaded={isLoaded}>
+                                    {item.tanggal}
+                                </Skeleton>
+                            </TableCell>
+                            <TableCell>
+                                <Skeleton isLoaded={isLoaded}>
+                                    {item.jumlahpengunjung}
+                                </Skeleton>
+                            </TableCell>
+                            <TableCell>
+                                <Skeleton isLoaded={isLoaded}>
+                                    {item.alamat}
+                                </Skeleton>
+                            </TableCell>
+                            <TableCell>
+                                <Skeleton isLoaded={isLoaded}>
+                                    <Button isIconOnly onPress={handleToEdit} className="text-primary bg-white border-primary border mr-2">
+                                        <LuPencilLine size={15} />
+                                    </Button>
+                                    <Button isIconOnly onPress={handleToEdit} className="text-danger bg-white border-danger border mr-2">
+                                        <LuTrash size={15} />
+                                    </Button>
+                                </Skeleton>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+
+            <div className="flex justify-center mt-5">
+                <Pagination total={1} initialPage={1} />
+            </div>
+        </div>
+    );
+}
