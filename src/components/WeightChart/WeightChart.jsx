01c177564e736cc97d241db213086ceea79bc5b2
Melati Alya Putri Irawan <melatialya.2021@student.uny.ac.id>
2024-12-16 12:59:45 +0700

    feat:[ma] user

--- src/components/WeightChart/WeightChart.jsx
+++ src/components/WeightChart/WeightChart.jsx
@@ -1,110 +1,79 @@
 "use client";
 
-import React from "react";
-import { Chart, CategoryScale, LinearScale, LineElement, PointElement, LineController, Title, Tooltip, Legend } from "chart.js";
+import React, { useEffect } from "react";
+import { Chart, CategoryScale, LinearScale, LineElement, PointElement, LineController, Title, Tooltip, Legend, Filler } from "chart.js";
+import { Line } from "react-chartjs-2";
 
 // Daftarkan skala dan elemen yang dibutuhkan
-Chart.register(CategoryScale, LinearScale, LineElement, PointElement, LineController, Title, Tooltip, Legend);
+Chart.register(CategoryScale, LinearScale, LineElement, PointElement, LineController, Title, Tooltip, Legend, Filler);
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      display: false,  // Menyembunyikan legend
+    },
+    title: {
+      display: true,
+      text: "Statistik Penimbangan Bayi Ananda Nadia",
+      color: "black",
+    },
+  },
+  scales: {
+    x: {
+      ticks: {
+        color: "rgba(0,0,0,.7)",
+      },
+      title: {
+        display: true,
+        text: "Tanggal",
+        color: "black",
+      },
+      grid: {
+        display: false,
+        borderDash: [2],
+        color: "rgba(33, 37, 41, 0.3)",
+      },
+    },
+    y: {
+      beginAtZero: true,
+      max: 12,
+      ticks: {
+        color: "rgba(0,0,0,.7)",
+      },
+      title: {
+        display: true,
+        text: "Berat Badan",
+        color: "black",
+      },
+      grid: {
+        borderDash: [3],
+        color: "rgba(0, 0, 0, 0.15)",
+      },
+    },
+  },
+};
+
+const data = {
+  labels: [
+    "Dec 31 2023", "Jan 21 2024", "Feb 11 2024", "Mar 03 2024", "Mar 24 2024",
+    "Apr 14 2024", "May 05 2024", "May 26 2024", "Jun 16 2024", "Jul 07 2024",
+    "Jul 28 2024", "Aug 18 2024", "Sep 08 2024", "Sep 29 2024", "Oct 20 2024",
+    "Nov 10 2024", "Dec 01 2024"
+  ],
+  datasets: [
+    {
+      backgroundColor: "rgba(49, 130, 206, 0.2)",
+      borderColor: "#18B3AB",
+      data: [6, 7, 7, 0, 8, 9, 0, 9, 9, 8, 9, 0, 8, 9, 10, 10, 11],
+      fill: true,  // Memungkinkan area bawah garis diisi dengan warna
+      tension: 0.4,  // Membuat garis lebih halus
+    },
+  ],
+};
 
 export default function WeightChart() {
-  React.useEffect(() => {
-    var config = {
-      type: "line",
-      data: {
-        labels: [
-          "Dec 31 2023",
-          "Jan 21 2024",
-          "Feb 11 2024",
-          "Mar 03 2024",
-          "Mar 24 2024",
-          "Apr 14 2024",
-          "May 05 2024",
-          "May 26 2024",
-          "Jun 16 2024",
-          "Jul 07 2024",
-          "Jul 28 2024",
-          "Aug 18 2024",
-          "Sep 08 2024",
-          "Sep 29 2024",
-          "Oct 20 2024",
-          "Nov 10 2024",
-          "Dec 01 2024",
-        ],
-        datasets: [
-          {
-            label: "Berat Badan",
-            backgroundColor: "#3182ce",
-            borderColor: "#3182ce",
-            data: [7, 7, 8, 0, 8, 9, 0, 8, 9, 9, 9, 10, 10, 9, 10, 0, 11],
-            fill: false,
-          },
-        ],
-      },
-      options: {
-        maintainAspectRatio: false,
-        responsive: true,
-        title: {
-          display: true,
-          text: "Statistik Penimbangan Bayi Ananda Nadia",
-          fontColor: "black",
-        },
-        legend: {
-          display: false,  // Menyembunyikan legend
-        },
-        tooltips: {
-          mode: "index",
-          intersect: false,
-        },
-        hover: {
-          mode: "nearest",
-          intersect: true,
-        },
-        scales: {
-          x: {
-            ticks: {
-              fontColor: "rgba(0,0,0,.7)",
-            },
-            display: true,
-            scaleLabel: {
-              display: true,
-              labelString: "Date",
-              fontColor: "black",
-            },
-            grid: {
-              display: false,
-              borderDash: [2],
-              color: "rgba(33, 37, 41, 0.3)",
-              zeroLineColor: "rgba(0, 0, 0, 0)",
-              zeroLineBorderDash: [2],
-            },
-          },
-          y: {
-            ticks: {
-              fontColor: "rgba(0,0,0,.7)",
-              beginAtZero: true,
-              max: 12,
-            },
-            display: true,
-            scaleLabel: {
-              display: true,
-              labelString: "Weight",
-              fontColor: "black",
-            },
-            grid: {
-              borderDash: [3],
-              color: "rgba(0, 0, 0, 0.15)",
-              zeroLineColor: "rgba(33, 37, 41, 0)",
-              zeroLineBorderDash: [2],
-            },
-          },
-        },
-      },
-    };
-
-    var ctx = document.getElementById("line-chart").getContext("2d");
-    new Chart(ctx, config);
-  }, []);
-
   return (
     <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white">
       <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
@@ -119,7 +88,7 @@ export default function WeightChart() {
       <div className="p-4 flex-auto">
         {/* Chart */}
         <div className="relative h-96">
-          <canvas id="line-chart"></canvas>
+          <Line data={data} options={options} />
         </div>
       </div>
     </div>
